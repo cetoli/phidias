34b1f3cf90cf12f98e9fcd20f7cf3f73ebb53104
sabrina.bettini <sabrina.bettini@c20c5121-2f45-0410-93d1-858c32205c50>
2008-02-24 19:14:32 +0000

    inclusao do metodo de troca de fundo

--- CriaConto/src/main/java/br/ufrj/nce/labase/phidias/view/Board.java
+++ CriaConto/src/main/java/br/ufrj/nce/labase/phidias/view/Board.java
@@ -9,13 +9,22 @@ import br.ufrj.nce.criaconto.images.Images;
 public class Board extends Playfield {
 	
 	private static final long serialVersionUID = 1L;
+	private Sprite backgroundImage;
 
 	public Board(Applet applet, int width, int height, String image) {
 		super(applet, width, height);
+		setBackgroundImage(image);		
+	}
+	
+	public void setBackgroundImage(String backgroundImageName) {
+		if (backgroundImage != null) {
+			removeSpriteFromList(backgroundImage);
+			removeSpritesFromListNow();
+		}
 		
-		Sprite backgroundImage = new Sprite(this);
+		backgroundImage = new Sprite(this);
     	backgroundImage.setBackground(true);
     	backgroundImage.setRectangular(true);
-    	backgroundImage.setImage(Images.createImage(image));
+    	backgroundImage.setImage(Images.createImage(backgroundImageName));
 	}
 }
