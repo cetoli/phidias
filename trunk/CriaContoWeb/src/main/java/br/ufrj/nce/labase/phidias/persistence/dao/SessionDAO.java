85e52dd2b9f36dbe46787b395d8d4321bf6e8c8d
sabrina.bettini <sabrina.bettini@c20c5121-2f45-0410-93d1-858c32205c50>
2008-02-25 02:25:58 +0000

    automatizacao da sessao

--- trunk/CriaContoWeb/src/main/java/br/ufrj/nce/labase/phidias/persistence/dao/SessionDAO.java
+++ trunk/CriaContoWeb/src/main/java/br/ufrj/nce/labase/phidias/persistence/dao/SessionDAO.java
@@ -1,7 +1,21 @@
 package br.ufrj.nce.labase.phidias.persistence.dao;
 
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
 import br.ufrj.nce.labase.phidias.persistence.model.Session;
 
 public class SessionDAO extends GenericDAO<Session> {
-
+	public Session findByStatus(int status){
+		try {
+			Query query = this.getSession().createQuery("select s from Session s where s.status = :status");
+			query.setParameter("status", status);
+			query.setMaxResults(1);
+			
+			Object session = query.getSingleResult();
+			return (Session) session;
+		} catch (NoResultException e) {
+			return null;
+		}
+	}
 }
