5f0a4c7750023a3b0194a7fa3b311529f86647b0
sabrina.bettini <sabrina.bettini@c20c5121-2f45-0410-93d1-858c32205c50>
2008-03-11 23:57:56 +0000

    alteracao das classes bases

--- trunk/CriaConto/src/main/java/br/ufrj/nce/labase/criaconto/view/attendant/CriaContoAttendant.java
+++ trunk/CriaConto/src/main/java/br/ufrj/nce/labase/criaconto/view/attendant/CriaContoAttendant.java
@@ -1,19 +1,14 @@
 package br.ufrj.nce.labase.criaconto.view.attendant;
 
-import java.applet.Applet;
 import java.awt.AWTEvent;
 import java.awt.BorderLayout;
 import java.awt.Button;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Frame;
-import java.awt.Graphics;
 import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Image;
 import java.awt.Insets;
 import java.awt.Label;
-import java.awt.Panel;
 import java.awt.TextArea;
 import java.awt.TextField;
 import java.awt.Toolkit;
@@ -21,104 +16,50 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 
-import javax.swing.JOptionPane;
-import javax.swing.Timer;
+import br.ufrj.nce.labase.phidias.view.attendant.Attendant;
 
-import br.ufrj.nce.labase.criaconto.images.Images;
-import br.ufrj.nce.labase.criaconto.control.Controller;
-import br.ufrj.nce.labase.criaconto.view.LoginPanel;
-import br.ufrj.nce.labase.phidias.communication.bean.EventResponseBean;
-import br.ufrj.nce.labase.phidias.controller.Session;
-import br.ufrj.nce.labase.phidias.exception.PhidiasException;
-
-public class CriaContoAttendant extends Applet {
+public class CriaContoAttendant extends Attendant {
 	private static final long serialVersionUID = 1L;
-	private Image backgroundImage;
+	private Color backgroundColor = new Color(249, 227, 203);
 	private TextArea commentsText;
 	private TextField stimulusText;
 	private TextArea comments;
 	private TextArea stimulus;
 	private TextArea moves;
-	private Timer movesTimer;
-	private LoginPanel loginPanel;
 	
-	private Color backgroundColor = new Color(249, 227, 203);
-	
-
 	public CriaContoAttendant() {
+		super("fundoAplicador.jpg");
 	}
 
 	public void init() {
-		setSize(1024, 820);
-		setBackground(Color.WHITE);
-		setLayout(new BorderLayout());
-		
-		loginPanel = new LoginPanel();
-    	loginPanel.setPreferredSize(new Dimension(1024, 820));
-    	add(loginPanel, BorderLayout.CENTER);
-    	
-    	loginPanel.addActionListener(new ActionListener() {
-    		public void actionPerformed(ActionEvent e) {
-    			if (registerSession()) {
-    				startApplication();
-    			} else {
-    				showMessageDialog("Ocorreu um erro ao iniciar o aplicativo! Por favor, tente novamente mais tarde!");
-    			}
-    		}
-    	});
+		super.init();
+	}	
+	
+	protected void appendMove(String move) {
+		moves.append(move + "\n");
 	}
 	
-	private void showMessageDialog(String message) {
-    	JOptionPane.showMessageDialog(this, message);	
-    }
-	
-	private boolean registerSession() {
-    	try {
-    		return Controller.registerSession(loginPanel.getLogin(), LoginPanel.CRIA_CONTO);
-    	} catch (PhidiasException ex) {
-			showMessageDialog("Erro ao iniciar sessão!");
-			return false;
-		}
-    }
-
-
-	private void startApplication() {
-		backgroundImage = Images.createImage("fundoAplicador.jpg");
-    	
-		setSize(1024, 820);
-    	removeAll();
-    	
-    	Panel p = new Panel() {
-    		public void paint(Graphics g) {
-    			super.paint(g);
-    			
-    			if (backgroundImage != null) {
-    				g.drawImage(backgroundImage, 0, 0, null);
-    			}
-    		}
-    	};
-    	
-    	p.setLayout(new GridBagLayout());
-    	p.setSize(1024, 820);
+	protected void startApplication() {
+		super.startApplication();
 		
 		Label movesLabel = new Label("Jogadas do paciente");
 		movesLabel.setBackground(backgroundColor);
-		p.add(movesLabel, new GridBagConstraints(0, 0, 3, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
+		mainPanel.add(movesLabel, new GridBagConstraints(0, 0, 3, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
 
 		moves = new TextArea();
 		moves.setRows(8);
 		moves.setColumns(80);
 		moves.setEditable(false);
-		p.add(moves, new GridBagConstraints(0, 1, 3, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
+		mainPanel.add(moves, new GridBagConstraints(0, 1, 3, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
 
 		Label commentsLabel = new Label("Entre com os comentarios");
 		commentsLabel.setBackground(backgroundColor);
-		p.add(commentsLabel, new GridBagConstraints(0, 2, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
+		mainPanel.add(commentsLabel, new GridBagConstraints(0, 2, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
 
 		commentsText = new TextArea();
 		commentsText.setColumns(80);
 		commentsText.setRows(3);
-		p.add(commentsText, new GridBagConstraints(1, 2, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
+		mainPanel.add(commentsText, new GridBagConstraints(1, 2, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
 
 		Button ok = new Button("OK");
 		ok.addActionListener(new ActionListener() {
@@ -126,20 +67,20 @@ public class CriaContoAttendant extends Applet {
 				registerComment();
 			}
 		});
-		p.add(ok, new GridBagConstraints(2, 2, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
+		mainPanel.add(ok, new GridBagConstraints(2, 2, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
 
 		comments = new TextArea();
 		comments.setRows(8);
 		comments.setColumns(80);
 		comments.setEditable(false);
-		p.add(comments, new GridBagConstraints(0, 3, 3, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
+		mainPanel.add(comments, new GridBagConstraints(0, 3, 3, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
 
 		Label interventionsLabel = new Label("Entre com os estimulos do NPC:");
 		interventionsLabel.setBackground(backgroundColor);
-		p.add(interventionsLabel, new GridBagConstraints(0, 4, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
+		mainPanel.add(interventionsLabel, new GridBagConstraints(0, 4, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
 
 		stimulusText = new TextField();
-		p.add(stimulusText, new GridBagConstraints(1, 4, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
+		mainPanel.add(stimulusText, new GridBagConstraints(1, 4, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
 
 		Button ok2 = new Button("OK");
 		ok2.addActionListener(new ActionListener() {
@@ -147,13 +88,13 @@ public class CriaContoAttendant extends Applet {
 				registerStimulus();
 			}
 		});
-		p.add(ok2, new GridBagConstraints(2, 4, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
+		mainPanel.add(ok2, new GridBagConstraints(2, 4, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
 
 		stimulus = new TextArea();
 		stimulus.setRows(8);
 		stimulus.setColumns(80);
 		stimulus.setEditable(false);
-		p.add(stimulus, new GridBagConstraints(0, 5, 3, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
+		mainPanel.add(stimulus, new GridBagConstraints(0, 5, 3, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
 		
 		Button changePhase = new Button("Mudar fase");
 		changePhase.addActionListener(new ActionListener() {
@@ -161,9 +102,8 @@ public class CriaContoAttendant extends Applet {
 				registerPhaseChange();
 			}
 		});
-		p.add(changePhase, new GridBagConstraints(0, 6, 3, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
-		
-		
+		mainPanel.add(changePhase, new GridBagConstraints(0, 6, 3, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
+				
 		Button exit = new Button("Sair");
 		exit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -172,57 +112,22 @@ public class CriaContoAttendant extends Applet {
 				}
 			}
 		});
-		p.add(exit, new GridBagConstraints(0, 7, 3, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
+		mainPanel.add(exit, new GridBagConstraints(0, 7, 3, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 0, 0));
 		
-		add(p, BorderLayout.CENTER);
-		
-		Session.getInstance().changePhase();
-				
-		movesTimer = new Timer(4000, new MovesTimer());
-		movesTimer.start();
-	}
-
-	private void registerComment() {
-		String comment = commentsText.getText();
-		try { 
-			if (Controller.registerComment(comment)) {
-				commentsText.setText("");
-				comments.append(comment + "\n");
-			}
-		} catch (PhidiasException ex) {
-			showMessageDialog("Erro ao inserir comentário!");
-		}
-	}
-
-	private void registerStimulus() {
-		String stimulusString = stimulusText.getText();
-		try {
-			if (Controller.registerStimulus(stimulusString)) {
-				stimulusText.setText("");
-				stimulus.append(stimulusString + "\n");
-			}
-		} catch (PhidiasException ex) {
-			showMessageDialog("Erro ao enviar estímulo!");
-		}
+		add(mainPanel, BorderLayout.CENTER);
 	}
 	
-	private boolean registerPhaseChange() {
-		boolean success = false;
-		try {
-			success = Controller.registerPhaseChange();
-			Session.getInstance().changePhase();
-		} catch (PhidiasException ex) {
-			showMessageDialog("Erro ao enviar solicitação de mudança de fase!");
-		}
-		return success;
+	protected void registerComment() {
+		if (super.registerComment(commentsText.getText())) {
+			comments.append(commentsText.getText() + "\n");	
+			commentsText.setText("");			
+		} 
 	}
 	
-	private boolean registerSessionEnd() {
-		try {
-			return Controller.registerSessionEnd(LoginPanel.CRIA_CONTO);
-		} catch (PhidiasException ex) {
-			showMessageDialog("Erro ao encerrar sessão!");
-			return false;
+	protected void registerStimulus() {
+		if (super.registerStimulus(stimulusText.getText())) {
+			stimulus.append(stimulusText.getText() + "\n");
+			stimulusText.setText("");
 		}
 	}
 
@@ -263,20 +168,5 @@ public class CriaContoAttendant extends Applet {
 		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
 		frame.setLocation((d.width - frame.getSize().width) / 2, (d.height - frame.getSize().height) / 2);
 		frame.setVisible(true);
-	}
-
-	private class MovesTimer implements ActionListener {
-		public void actionPerformed(ActionEvent arg0) {		
-			try {
-				EventResponseBean response = Controller.getMoves();
-				if (response != null && response.getMoves() != null && response.getMoves().size() > 0) {
-					for (String move : response.getMoves()) {
-						moves.append(move + "\n");
-					}
-				}
-			} catch (PhidiasException ex) {
-				showMessageDialog("Erro ao recuperar as jogadas do paciente!");
-			}
-		}
 	}
 }
