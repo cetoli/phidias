6b5307040b2074a371568397e5c6b58cb17830e6
sabrina.bettini <sabrina.bettini@c20c5121-2f45-0410-93d1-858c32205c50>
2008-04-22 16:10:59 +0000

    separacao do criaconto do phidias

---
+++ trunk/Phidias/src/main/java/br/ufrj/nce/labase/phidias/view/player/GameStartTimer.java
@@ -0,0 +1,10 @@
+package br.ufrj.nce.labase.phidias.view.player;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameStartTimer implements ActionListener {				
+	public void actionPerformed(ActionEvent arg0) {	
+		
+	}
+}
