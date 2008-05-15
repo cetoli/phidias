5944514944285b03fed627192297a3a6bf3b86e2
sabrina.bettini <sabrina.bettini@c20c5121-2f45-0410-93d1-858c32205c50>
2008-05-15 22:17:33 +0000

    exclusao do diretorio bin

--- Phidias/src/main/java/br/ufrj/nce/labase/phidias/persistence/dao/SessionGamePhaseDAO.java
+++
@@ -1,7 +0,0 @@
-package br.ufrj.nce.labase.phidias.persistence.dao;
-
-import br.ufrj.nce.labase.phidias.persistence.model.SessionGamePhase;
-
-public class SessionGamePhaseDAO extends GenericDAO<SessionGamePhase> {
-
-}
