107d673c8697e4639f8e70b2262c75bc1d65bda5
andrelu@hotmail.com <andrelu@hotmail.com@c20c5121-2f45-0410-93d1-858c32205c50>
2008-06-04 00:57:44 +0000

    Refactoring que retira o Filter do SpriteManager para o Sprite. Criação de obstaculos não-sprites no SpriteManager.

--- Phidias/src/main/java/br/ufrj/nce/labase/phidias/toolkit/sprite/Sprite.java
+++ Phidias/src/main/java/br/ufrj/nce/labase/phidias/toolkit/sprite/Sprite.java
@@ -10,21 +10,23 @@ import java.util.List;
 
 import br.ufrj.nce.labase.common.MidiSound;
 import br.ufrj.nce.labase.phidias.controller.Controller;
+import br.ufrj.nce.labase.phidias.toolkit.filter.GraphicFilter;
 import br.ufrj.nce.labase.phidias.toolkit.mapping.Mapping;
 import br.ufrj.nce.labase.phidias.toolkit.sprite.event.SpriteActionEvent;
 import br.ufrj.nce.labase.phidias.toolkit.sprite.event.SpriteActionListener;
 import br.ufrj.nce.labase.phidias.util.Images;
 
 /**
- * Sprites, by default, implement facilities for drag events.
- * If some instance may not be dragged, or might implement its own dragging rules, so it must override
- * <code>mousePressed</code>, <code>mouseDragged</code> and <code>mouseReleased</code> methods, or
- * in simpler cases, just call setDragEnabled(false).
- *  
+ * Sprites, by default, implement facilities for drag events. If some instance
+ * may not be dragged, or might implement its own dragging rules, so it must
+ * override <code>mousePressed</code>, <code>mouseDragged</code> and
+ * <code>mouseReleased</code> methods, or in simpler cases, just call
+ * setDragEnabled(false).
+ * 
  * @author Andre Moraes
  * @author Diogo Gomes
  * @author Sabrina Bettini
- *
+ * 
  */
 public class Sprite {
 
@@ -38,58 +40,70 @@ public class Sprite {
 	private String name;
 
 	private List<SpriteActionListener> listeners = new ArrayList<SpriteActionListener>();
+
+	/**
+	 * List of Graphic filters to be applied on Sprites on paint method, when
+	 * spriteHoverEnabled attribute is set to true.
+	 */
+	private List<GraphicFilter> hoverFilters = new ArrayList<GraphicFilter>();
+
 	/**
 	 * Current position for Sprite instance
 	 */
 	private Point2D coordinate;
-	
+
 	/**
-	 * Original instance position, before any drag events. This coordinate is used to return the Sprite
-	 * back to its original position in cases that the drag event is not valid, according to business rules.
+	 * Original instance position, before any drag events. This coordinate is
+	 * used to return the Sprite back to its original position in cases that the
+	 * drag event is not valid, according to business rules.
 	 */
 	private Point2D origin;
 
 	/**
-	 * Mapping associated to this instance. Mapping represents a Rectangle area into where drag events
-	 * are valid to this Sprite instance. In case of null mapping object, it means that there are no rules
-	 * defined for mapping, and any area will permit an object to be placed in.<br>
-	 * When mapping validations return false to sprite instance, it will be then return to its original position.
+	 * Mapping associated to this instance. Mapping represents a Rectangle area
+	 * into where drag events are valid to this Sprite instance. In case of null
+	 * mapping object, it means that there are no rules defined for mapping, and
+	 * any area will permit an object to be placed in.<br>
+	 * When mapping validations return false to sprite instance, it will be then
+	 * return to its original position.
 	 */
 	private Mapping mapping;
-	
+
 	private boolean dragEnabled;
-	
+
 	private boolean visible = true;
-	
+
 	/**
-	 * Sets if the sprite must be disabled from attending mouse events after a drag event that receives
-	 * a true response from this corresponding mapping.
+	 * Sets if the sprite must be disabled from attending mouse events after a
+	 * drag event that receives a true response from this corresponding mapping.
 	 */
-	//TODO:
+	// TODO:
 	private boolean disableDragAfterHit = false;
-	
+
 	/**
-	 * Sets if the sprite must be moved to its Mapped destination, after a mousReleased
-	 * event that returns a true value from its corresponding Mapping instance.
+	 * Sets if the sprite must be moved to its Mapped destination, after a
+	 * mousReleased event that returns a true value from its corresponding
+	 * Mapping instance.
 	 */
 	private boolean snapToDestination = false;
-	
+
 	/**
-	 * Last position coordinate, used to track drag events and render the image in correct position,
-	 * as mouse moves through screen.
-	 * It is set by mousePressed.
+	 * Last position coordinate, used to track drag events and render the image
+	 * in correct position, as mouse moves through screen. It is set by
+	 * mousePressed.
 	 */
 	private Point2D lastPositionBeforeDrag;
-	
+
 	private int width, height;
 
 	private SpriteManager spriteManager;
 
-	//TODO: verificar se é viável colocar "Mapping... mapping" no construtor, aceitando um conjunto de mapeamentos.
+	// TODO: verificar se é viável colocar "Mapping... mapping" no construtor,
+	// aceitando um conjunto de mapeamentos.
 	public Sprite(SpriteManager spriteManager, Point2D coordinate, BufferedImage image) {
 		this.spriteManager = spriteManager;
 		this.coordinate = coordinate;
-		this.origin 	= (Point2D) coordinate.clone();
+		this.origin = (Point2D) coordinate.clone();
 		this.image = image;
 		if (image != null) {
 			width = image.getWidth();
@@ -115,7 +129,7 @@ public class Sprite {
 		this(spriteManager, coordinate, getImage(imagePath));
 		this.setMapping(mapping);
 	}
-	
+
 	public Sprite(SpriteManager spriteManager, Point2D coordinate, int width, int height, String imagePath) {
 		this(spriteManager, coordinate, getImage(imagePath));
 		this.width = width;
@@ -130,33 +144,36 @@ public class Sprite {
 
 	/**
 	 * Adds a SpriteActionListener to the list of listeners.
-	 *  
-	 * @param listener the SpriteActionListener to be added.
+	 * 
+	 * @param listener
+	 *            the SpriteActionListener to be added.
 	 */
 	public void addSpriteActionListener(SpriteActionListener listener) {
 		if (!listeners.contains(listener)) {
 			listeners.add(listener);
 		}
 	}
-	
+
 	/**
 	 * Removes a SpriteActionListener from the list of listeners.
-	 *  
-	 * @param listener the SpriteActionListener to be removed.
+	 * 
+	 * @param listener
+	 *            the SpriteActionListener to be removed.
 	 */
 	public void removeSpriteActionListener(SpriteActionListener listener) {
 		listeners.remove(listener);
 	}
-	
+
 	/**
-	 * Fires a SpriteActionEvent to all SpriteActionListeners added to this Sprite.
+	 * Fires a SpriteActionEvent to all SpriteActionListeners added to this
+	 * Sprite.
 	 */
 	private void fireSpriteActionEvent(SpriteActionEvent event) {
 		for (SpriteActionListener l : listeners) {
 			l.spriteActionPerformed(event);
 		}
 	}
-	
+
 	public static BufferedImage getImage(String imagePath) {
 		return Images.getBufferedImage(imagePath);
 	}
@@ -172,116 +189,119 @@ public class Sprite {
 	public List<Sprite> objectsCollided() {
 		return this.spriteManager.spritesCollided(this);
 	}
-	
- 	/**
- 	 * Default implementation for mouse drag events.
- 	 * Changes current instance coordinates, according to drag event. New values
- 	 * will be used by thread's paint() method, to render image in correct position.
- 	 * In case of particular treatment of this event is needed, this method must be overriden by subclass.
- 	 * 
- 	 */
- 	public void mouseDragged(MouseEvent e){
- 		if (this.dragEnabled){
- 			this.setPosXY(e.getX() - this.lastPositionBeforeDrag.getX(),
- 					e.getY() - this.lastPositionBeforeDrag.getY());		
- 		}
- 		
- 		SpriteActionEvent event = new SpriteActionEvent(this, SpriteActionEvent.MOUSE_DRAGGED);
- 		fireSpriteActionEvent(event);
+
+	/**
+	 * Default implementation for mouse drag events. Changes current instance
+	 * coordinates, according to drag event. New values will be used by thread's
+	 * paint() method, to render image in correct position. In case of
+	 * particular treatment of this event is needed, this method must be
+	 * overriden by subclass.
+	 * 
+	 */
+	public void mouseDragged(MouseEvent e) {
+		if (this.dragEnabled) {
+			this.setPosXY(e.getX() - this.lastPositionBeforeDrag.getX(), e.getY() - this.lastPositionBeforeDrag.getY());
+		}
+
+		SpriteActionEvent event = new SpriteActionEvent(this, SpriteActionEvent.MOUSE_DRAGGED);
+		fireSpriteActionEvent(event);
 	}
 
- 	/**
- 	 * Default implementation for mouse pressed events.
- 	 * Method called by SpriteManager only for instance whose coordinates corresponds to the mouse area interation
- 	 * where click occurred.
- 	 */
- 	public void mousePressed(MouseEvent e){
-		this.lastPositionBeforeDrag.setLocation(e.getX() - this.getPosX(),
-												e.getY() - this.getPosY());
-		moveStartTime = System.currentTimeMillis();	
-		
+	/**
+	 * Default implementation for mouse pressed events. Method called by
+	 * SpriteManager only for instance whose coordinates corresponds to the
+	 * mouse area interation where click occurred.
+	 */
+	public void mousePressed(MouseEvent e) {
+		this.lastPositionBeforeDrag.setLocation(e.getX() - this.getPosX(), e.getY() - this.getPosY());
+		moveStartTime = System.currentTimeMillis();
+
 		SpriteActionEvent event = new SpriteActionEvent(this, SpriteActionEvent.MOUSE_PRESSED);
- 		fireSpriteActionEvent(event);
- 	}
- 	
- 	/**
- 	 * Default implementation for mouse Released events.
- 	 * @param e
- 	 */
- 	public void mouseReleased(MouseEvent e){
- 		// actions only when a mapping is defined
- 		if (this.dragEnabled && this.mapping != null){
- 			if (this.mapping.intersect()){
- 				if (this.disableDragAfterHit){
- 					this.dragEnabled = false;
- 				}
- 				if (this.snapToDestination){
- 					this.coordinate.setLocation(mapping.getSpriteDestination());
- 				}
- 			}else {
- 				this.resetCoordinates();
- 			}
- 		}
- 		
- 		moveEndTime = System.currentTimeMillis();
- 		
- 		SpriteActionEvent event = new SpriteActionEvent(this, SpriteActionEvent.MOUSE_RELEASED);
- 		fireSpriteActionEvent(event);
- 	}
- 	
- 	/** 
- 	 * Returns the total time of the sprite drag movement. The movement total time is the time
- 	 * between the mouse being pressed and the mouse being released. In other words, the time
- 	 * of the drag movement.
- 	 *
- 	 * @return the total drag movement time of the Sprite in seconds.
- 	 */
- 	private long getMoveTime() {
-		return (moveEndTime - moveStartTime)/1000;
-	}
- 	
- 	/**
- 	 * Resets the coordinated of the Sprite to its original coordinates.
- 	 */
- 	private void resetCoordinates(){
- 		this.coordinate.setLocation(this.origin);
- 	}
-
-
- 	/**
- 	 * Default empty implementation. Must be overridden by subclasses.
- 	 * @param e
- 	 */
- 	public void mouseExited(MouseEvent e){
- 		SpriteActionEvent event = new SpriteActionEvent(this, SpriteActionEvent.MOUSE_EXITED);
- 		fireSpriteActionEvent(event);
+		fireSpriteActionEvent(event);
 	}
 
- 	/**
- 	 * Default empty implementation. Must be overridden by subclasses.
- 	 * @param e
- 	 */
-	public void mouseMoved(MouseEvent e){
+	/**
+	 * Default implementation for mouse Released events.
+	 * 
+	 * @param e
+	 */
+	public void mouseReleased(MouseEvent e) {
+		// actions only when a mapping is defined
+		if (this.dragEnabled && this.mapping != null) {
+			if (this.mapping.intersect()) {
+				if (this.disableDragAfterHit) {
+					this.dragEnabled = false;
+				}
+				if (this.snapToDestination) {
+					this.coordinate.setLocation(mapping.getSpriteDestination());
+				}
+			} else {
+				this.resetCoordinates();
+			}
+		}
+
+		moveEndTime = System.currentTimeMillis();
+
+		SpriteActionEvent event = new SpriteActionEvent(this, SpriteActionEvent.MOUSE_RELEASED);
+		fireSpriteActionEvent(event);
+	}
+
+	/**
+	 * Returns the total time of the sprite drag movement. The movement total
+	 * time is the time between the mouse being pressed and the mouse being
+	 * released. In other words, the time of the drag movement.
+	 * 
+	 * @return the total drag movement time of the Sprite in seconds.
+	 */
+	private long getMoveTime() {
+		return (moveEndTime - moveStartTime) / 1000;
+	}
+
+	/**
+	 * Resets the coordinated of the Sprite to its original coordinates.
+	 */
+	private void resetCoordinates() {
+		this.coordinate.setLocation(this.origin);
+	}
+
+	/**
+	 * Default empty implementation. Must be overridden by subclasses.
+	 * 
+	 * @param e
+	 */
+	public void mouseExited(MouseEvent e) {
+		SpriteActionEvent event = new SpriteActionEvent(this, SpriteActionEvent.MOUSE_EXITED);
+		fireSpriteActionEvent(event);
+	}
+
+	/**
+	 * Default empty implementation. Must be overridden by subclasses.
+	 * 
+	 * @param e
+	 */
+	public void mouseMoved(MouseEvent e) {
 		SpriteActionEvent event = new SpriteActionEvent(this, SpriteActionEvent.MOUSE_MOVED);
- 		fireSpriteActionEvent(event);
+		fireSpriteActionEvent(event);
 	}
 
- 	/**
- 	 * Default empty implementation. Must be overridden by subclasses.
- 	 * @param e
- 	 */
-	public void mouseEntered(MouseEvent e){
+	/**
+	 * Default empty implementation. Must be overridden by subclasses.
+	 * 
+	 * @param e
+	 */
+	public void mouseEntered(MouseEvent e) {
 		SpriteActionEvent event = new SpriteActionEvent(this, SpriteActionEvent.MOUSE_ENTERED);
- 		fireSpriteActionEvent(event);
+		fireSpriteActionEvent(event);
 	}
 
- 	/**
- 	 * Default empty implementation. Must be overridden by subclasses.
- 	 * @param e
- 	 */
-	public void mouseClicked(MouseEvent e){
+	/**
+	 * Default empty implementation. Must be overridden by subclasses.
+	 * 
+	 * @param e
+	 */
+	public void mouseClicked(MouseEvent e) {
 		SpriteActionEvent event = new SpriteActionEvent(this, SpriteActionEvent.MOUSE_CLICKED);
- 		fireSpriteActionEvent(event);
+		fireSpriteActionEvent(event);
 	}
 
 	public Point2D getCoordinate() {
@@ -306,7 +326,10 @@ public class Sprite {
 
 	public void setMapping(Mapping mapping) {
 		this.mapping = mapping;
-		/* backward referente in mapping instance, pointing to this sprite instance */
+		/*
+		 * backward referente in mapping instance, pointing to this sprite
+		 * instance
+		 */
 		mapping.setSprite(this);
 	}
 
@@ -331,17 +354,18 @@ public class Sprite {
 	}
 
 	/**
-	 * Sets if the sprite must be moved to its Mapped destination, after a mousReleased
-	 * event that returns a true value from its corresponding Mapping instance.
+	 * Sets if the sprite must be moved to its Mapped destination, after a
+	 * mousReleased event that returns a true value from its corresponding
+	 * Mapping instance.
 	 */
 	public void setSnapToDestination(boolean snapToDestination) {
 		this.snapToDestination = snapToDestination;
 	}
 
-	public BufferedImage getImage(){
+	public BufferedImage getImage() {
 		return this.image;
 	}
-	
+
 	public double getPosX() {
 		return coordinate.getX();
 	}
@@ -350,10 +374,10 @@ public class Sprite {
 		return coordinate.getY();
 	}
 
-	public void setPosXY(double x, double y){
+	public void setPosXY(double x, double y) {
 		this.coordinate.setLocation(x, y);
 	}
-	
+
 	public SpriteManager getSpriteManager() {
 		return spriteManager;
 	}
@@ -385,7 +409,7 @@ public class Sprite {
 	public boolean isPlaySound() {
 		return playSound;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -393,39 +417,63 @@ public class Sprite {
 	public String getName() {
 		return name;
 	}
-	
-	/** 
+
+	public List<GraphicFilter> getHoverFilters() {
+		return hoverFilters;
+	}
+
+	public void setHoverFilters(List<GraphicFilter> hoverFilters) {
+		this.hoverFilters = hoverFilters;
+	}
+
+	/**
 	 * Register in the server database that the user gave up using this Sprite.
 	 */
 	public void registerGiveUpEvent() {
-		Controller.registerGiveUpEvent(getMoveTime(), getName());	
+		Controller.registerGiveUpEvent(getMoveTime(), getName());
 	}
-	
-	/** 
-	 * Register in the server database that the user removed this Sprite from the board.
+
+	/**
+	 * Register in the server database that the user removed this Sprite from
+	 * the board.
 	 */
 	public void registerTakeFromBoardEvent() {
 		Controller.registerTakeFromSceneEvent(getMoveTime(), getName());
 	}
-	
-	/** 
-	 * Register in the server database that the user put this Sprite on the board.
+
+	/**
+	 * Register in the server database that the user put this Sprite on the
+	 * board.
 	 */
 	public void registerPutOnBoardEvent() {
 		Controller.registerPutOnSceneEvent(getMoveTime(), getName());
 	}
-	
-	/** 
-	 * Register in the server database that the user moved this Sprite on the board.
+
+	/**
+	 * Register in the server database that the user moved this Sprite on the
+	 * board.
 	 */
 	public void registerMoveOnBoardEvent() {
-		Controller.registerMoveOnSceneEvent(getMoveTime(), getName());	
+		Controller.registerMoveOnSceneEvent(getMoveTime(), getName());
 	}
-	
-	/** 
-	 * Register in the server database that this Sprite collided with another Sprite.
+
+	/**
+	 * Register in the server database that this Sprite collided with another
+	 * Sprite.
 	 */
 	public void registerCollisionEvent(Sprite sprite) {
-		Controller.registerCollisionEvent(getName(), sprite.getName());	
+		Controller.registerCollisionEvent(getName(), sprite.getName());
+	}
+
+	/**
+	 * Register a GraphicFilter to handle a Image Sprite.
+	 * @param o
+	 * @return
+	 */
+	public boolean addGraphicFilter(GraphicFilter o) {
+		if (this.hoverFilters == null)
+			this.hoverFilters = new ArrayList<GraphicFilter>();
+
+		return hoverFilters.add(o);
 	}
 }
