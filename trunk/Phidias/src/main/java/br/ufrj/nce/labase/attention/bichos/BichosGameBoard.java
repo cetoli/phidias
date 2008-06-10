e54cace3adef98daf0947794c3f68b31812a5ba7
diogosmg <diogosmg@c20c5121-2f45-0410-93d1-858c32205c50>
2008-06-10 23:19:49 +0000

    Disponibilizando jogo dos bichos. Correção na classe Images referente à instanciação de uma imagem a partir de um arquivo, passando a utilizar getResourceAsStream()

--- trunk/Phidias/src/main/java/br/ufrj/nce/labase/attention/bichos/BichosGameBoard.java
+++ trunk/Phidias/src/main/java/br/ufrj/nce/labase/attention/bichos/BichosGameBoard.java
@@ -4,7 +4,6 @@ import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.geom.Point2D;
 
-import br.ufrj.nce.labase.phidias.toolkit.sprite.NPC;
 import br.ufrj.nce.labase.phidias.toolkit.PickingGameBoard;
 
 public class BichosGameBoard extends PickingGameBoard{
@@ -14,30 +13,46 @@ public class BichosGameBoard extends PickingGameBoard{
 	public void initPickingGame(){
 		this.setBackgroundImage("background.gif");
 
+		this.createSprite(new Point(382,111), "abelha1.gif");
+		this.createSprite(new Point(182,277), "aranha-1.gif");
+		this.createSprite(new Point(43,334), "avestruz.gif");
+		this.createSprite(new Point(190,325), "avestruz1.gif");
+		this.createSprite(new Point(455,150), "bambi1.gif");
+		this.createSprite(new Point(94,200), "bicho.gif");
+		this.createSprite(new Point(276,206), "bicho2.gif");
+		this.createSprite(new Point(140,63), "camelo1.gif");
+		this.createSprite(new Point(39,240), "canguru.gif");
+		
+		this.createSprite(new Point(256,247), "caracol.gif");
+		this.createSprite(new Point(405,353), "caranguejo1.gif");
+		this.createSprite(new Point(326,225), "coelho-1.gif");
+		this.createSprite(new Point(440,28), "coruja-1.gif");
+		this.createSprite(new Point(90,136), "elefante1.gif");
+		this.createSprite(new Point(352,174), "esquilo-1.gif");
+		
 		this.createSprite(new Point(240,120), "esquilo2.gif");
-		this.createSprite(new Point(212,256), "foca1.gif");
-		this.createSprite(new Point(404,200), "galo-1.gif");
-		this.createSprite(new Point(90,306), "gamba-1.gif");
-		this.createSprite(new Point(30,19), "girafa1.gif");
-		this.createSprite(new Point(146,355), "jacare1.gif");
-		this.createSprite(new Point(38,170), "leao1.gif");
-		this.createSprite(new Point(401,120), "lobo-1.gif");
-		this.createSprite(new Point(285,58), "mico-1.gif");
-		this.createSprite(new Point(260,354), "minhoca-1.gif");
-		this.createSprite(new Point(113,361), "minoca2.gif");
-		this.createSprite(new Point(120,30), "morcego-1.gif");
-		this.createSprite(new Point(108,241), "onca.gif");
-		this.createSprite(new Point(256,136), "panda-1.gif");
+		this.createSprite(new Point(214,269), "foca1.gif");
+		this.createSprite(new Point(418,202), "galo-1.gif");
+		this.createSprite(new Point(97,306), "gamba-1.gif");
+		this.createSprite(new Point(324,179), "guaxinim.gif");
+		this.createSprite(new Point(30,18), "girafa1.gif");
+		this.createSprite(new Point(38,178), "leao1.gif");
+		this.createSprite(new Point(388,134), "lobo-1.gif");
+		this.createSprite(new Point(285,50), "mico-1.gif");
+		this.createSprite(new Point(272,374), "minhoca-1.gif");
+		this.createSprite(new Point(140,326), "minoca2.gif");
+		this.createSprite(new Point(111,368), "porco.gif");
+		this.createSprite(new Point(132,355), "jacare1.gif");
+		this.createSprite(new Point(108,42), "morcego-1.gif");
+		this.createSprite(new Point(91,256), "onca.gif");
+		this.createSprite(new Point(257,136), "panda-1.gif");
 		this.createSprite(new Point(372,12), "passarinho-1.gif");
 		this.createSprite(new Point(315,300), "peixe-1.gif");
-		this.createSprite(new Point(444,372), "rato-1.gif");
+		this.createSprite(new Point(434,367), "rato-1.gif");
 		this.createSprite(new Point(175,202), "rino-1.gif");
-		this.createSprite(new Point(144,310), "tartaruga-1.gif");
-		this.createSprite(new Point(223,270), "bicho2.gif");
-		this.createSprite(new Point(365,368), "tatu.gif");
-		this.createSprite(new Point(313,162), "guaxinim.gif");
-		this.createSprite(new Point(117,351), "porco.gif");
-		this.createSprite(new Point(325,101), "urso1.gif");
+		this.createSprite(new Point(154,297), "tartaruga-1.gif");
+		this.createSprite(new Point(346,366), "tatu.gif");
+		this.createSprite(new Point(335,99), "urso1.gif");
 		this.createSprite(new Point(193,132), "veado1.gif");
 		this.createSprite(new Point(75,92), "zebra-1.gif");
 		
