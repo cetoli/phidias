ee2eacf5a9d814b14550d31a10c8091acd99bb00
sabrina.bettini <sabrina.bettini@c20c5121-2f45-0410-93d1-858c32205c50>
2008-06-14 17:13:36 +0000

    alteracao do nome da variavel de fase

--- Phidias/src/main/java/br/ufrj/nce/labase/attention/castelo/CasteloGameBoard.java
+++ Phidias/src/main/java/br/ufrj/nce/labase/attention/castelo/CasteloGameBoard.java
@@ -110,8 +110,8 @@ public class CasteloGameBoard extends PickingGameBoard implements SpriteActionLi
 	}
 	
 	private void nextPhase(){
-		this.CURRENT_PHASE++;
-		System.out.println("Mudando para a fase: "+CURRENT_PHASE);
-		this.changePhase(this.CURRENT_PHASE);
+		this.currentPhase++;
+		System.out.println("Mudando para a fase: "+currentPhase);
+		this.changePhase(this.currentPhase);
 	}
 }
