3f52a784ce596f1d5f4f6207918d675d92a448aa
lucioamp <lucioamp@c20c5121-2f45-0410-93d1-858c32205c50>
2009-08-01 00:17:46 +0000

    Novos recursos REST

---
+++ PhidiasREST/src/br/ufrj/nce/labase/phidias/resources/ActionTypeResource.java
@@ -0,0 +1,43 @@
+package br.ufrj.nce.labase.phidias.resources;
+
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.StreamingOutput;
+
+import br.ufrj.nce.labase.phidias.persistence.dao.ActionTypeDAO;
+import br.ufrj.nce.labase.phidias.persistence.model.ActionType;
+
+@Path("/tipoacao")
+public class ActionTypeResource {
+
+	@GET()
+	@Path("/jogo/{gameId}")
+	@Produces("text/xml")
+	public StreamingOutput getGameActionTypes(@PathParam ("gameId") Long gameId) {
+		ActionTypeDAO actionTypeDAO = new ActionTypeDAO();
+		final List<ActionType> actionTypeList = actionTypeDAO.getGameActionTypes(gameId);
+		
+		return new StreamingOutput() {
+			public void write(OutputStream outputStream) {
+				PrintWriter out = new PrintWriter(outputStream);
+				out.println("<?xml version=\"1.0\"?>");
+				out.println("<tipoacoes>");
+
+				for (ActionType actionType : actionTypeList) {
+					out.print("  <tipoacao id=\"" + actionType.getId()
+							+ "\" nome=\"" + actionType.getDescription() + "\"/>");
+				}
+				
+				out.println("</tipoacoes>");
+				
+				out.close();
+			}
+		};
+	}
+}
--- PhidiasREST/src/br/ufrj/nce/labase/phidias/resources/HelloWorldResource.java
+++ PhidiasREST/src/br/ufrj/nce/labase/phidias/resources/HelloWorldResource.java
@@ -14,18 +14,6 @@ import br.ufrj.nce.labase.phidias.persistence.model.Patient;
 //The Java class will be hosted at the URI path "/helloworld"
 @Path("/")
 public class HelloWorldResource {
-	// The Java method will process HTTP GET requests
-//	@GET
-	// The Java method will produce content identified by the MIME Media
-	// type "text/plain"
-//	@Produces("text/plain")
-//	public String getClichedMessage() {
-//		PatientDAO pDAO = new PatientDAO();
-//		pDAO.findAll();
-//		
-//		// Return some cliched textual content
-//		return "Hello World";
-//	}
 	@GET
 	@Produces("text/xml")
 	@Path("/paciente")
